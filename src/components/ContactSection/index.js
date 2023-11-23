46d967c2ea1d85cf890e2896d82e7b035a74c891
Dinesh <dineshbabum17d@gmail.com>
2023-11-23 18:20:23 +0530

    update

--- src/components/ContactSection/index.js
+++ src/components/ContactSection/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Contact.css";
-import { BiLogoGmail, BiSolidPhoneCall } from 'react-icons/bi'
+import { BiDockRight, BiLogoGmail, BiSolidPhoneCall } from 'react-icons/bi'
 import { MdLocationOn } from 'react-icons/md'
 import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
@@ -19,6 +19,30 @@ function Contact() {
         </a>
       </div>
       <div className="mail-contact mb-5">
+      <span className="float-left">
+     
+          <a
+            className="btn btn-sm btn-success "
+            target="_blank"
+            href="https://drive.google.com/file/d/13iScuSSUq9bJ4fdUzZmxXrWdzFTqQs13/view?usp=sharing"
+            rel="noreferrer">
+            Rusume < BiDockRight />
+          </a>
+
+          &emsp;
+          <a
+            href="https://www.linkedin.com/in/dinesh-babu-mathivanan-264228277/"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-sm btn-success "
+          >
+            <FaLinkedin />
+          </a>
+          &emsp;
+        </span>
+      </div>
+ 
+      <div className="mail-contact mb-5">
         <div className="contacts p-4 text-center">
           <BiLogoGmail /> <span>dineshbabum17d@gmail.com</span>
         </div>
@@ -31,7 +55,7 @@ function Contact() {
         </div>
       </div>
       <div className="footer mt-3">
-        <span className="float-left">
+      <span className="float-left">
           <a
             href="https://github.com/DineshMathi17"
             target="_blank"
@@ -41,16 +65,8 @@ function Contact() {
             < FaGithub />
           </a>
           &emsp;
-          <a
-            href="https://www.linkedin.com/in/dinesh-babu-mathivanan-264228277/"
-            target="_blank"
-            rel="noreferrer"
-            className="btn btn-sm btn-success "
-          >
-            <FaLinkedin />
-          </a>
-          &emsp;
-        </span>
+   
+        </span> 
         <span>Copyright © 2023. </span>&emsp;{" "}
         <span> All Rights Reserved by Dinesh </span>
       </div>
